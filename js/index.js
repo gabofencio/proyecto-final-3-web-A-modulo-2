95f9fe13b3ac835f92f83e4a82ca26f5f0a51b82
Gabriel <smurfjap@hotmail.com>
2026-03-19 20:06:45 -0600

    feature:swap de photocards grid a photocards en carousel

--- js/index.js
+++ js/index.js
@@ -1,14 +1,40 @@
-var swiper = new Swiper(".my-swiper", {
-  slidesPerView: 3,
-  spaceBetween: 20,
-  loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    600: { slidesPerView: 1 },
-    900: { slidesPerView: 2 },
-    1200: { slidesPerView: 3 },
-  },
+const track = document.querySelector('.carousel-track');
+const slides = document.querySelectorAll('.carousel-slide');
+const nextButton = document.querySelector('.next');
+const prevButton = document.querySelector('.prev');
+const dotsContainer = document.querySelector('.dots');
+
+let index = 0;
+
+slides.forEach((_, i) => {
+    const dot = document.createElement('button');
+    dot.addEventListener('click', () => goToSlide(i));
+    dotsContainer.appendChild(dot);
 });
+
+const dots = document.querySelectorAll('.dots button');
+function updateDots() {
+    dots.forEach(dot => dot.classList.remove('active'));
+    dots[index].classList.add('active');
+}
+
+function goToSlide(i) {
+    index = i;
+    track.scrollTo({
+        left: slides[i].offsetLeft,
+        behavior: 'smooth'
+    });
+    updateDots();
+}
+
+nextButton.addEventListener('click', () => {
+    index = (index + 1) % slides.length;
+    goToSlide(index);
+});
+
+prevButton.addEventListener('click', () => {
+    index = (index - 1 + slides.length) % slides.length;
+    goToSlide(index);
+});
+
+updateDots();
